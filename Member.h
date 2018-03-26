f381650871481666c694463c8ef1df987aafcf35
yarin665 <yarin665@gmail.com>
2018-03-26 15:49:48 +0300

    Upload first program

---
+++ Member.h
@@ -0,0 +1,95 @@
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#ifndef TASK3C_MEMBER_H
+#define TASK3C_MEMBER_H
+
+#endif //TASK3C_MEMBER_H
+using namespace std;
+
+
+class Member{
+
+
+public:
+
+    //Constructor
+    Member(){
+        id=idCounter++;
+        ++counter;
+    }
+
+    //Destructor
+    ~Member() {
+        --counter;
+    }
+
+    //Follow someone
+    void follow(Member &x){
+        bool flag=true;
+        //Check if you not following this person already
+        for (int i : UfollowStore) {
+            if (i ==x.id) flag= false;
+        }
+        //If you don't follow -
+        if (flag){
+            //Add him to your following list
+            UfollowStore.push_back(x.id);
+            //Add you to his followers list
+            x.followUStore.push_back(id);
+        }
+
+
+    }
+
+    //Unfollow someone
+    void unfollow(Member &x){
+
+        bool flag=false;
+        //Check if you follow this person
+        for (int i : UfollowStore) {
+            if (i ==x.id) {
+                flag=true;
+            }
+        }
+        if(flag){
+
+            //Remove from your list this person
+            UfollowStore.erase(remove(UfollowStore.begin(), UfollowStore.end(), x.id)
+                    , UfollowStore.end());
+
+            //Remove you from the followers list of this person
+            x.followUStore.erase(remove(x.followUStore.begin(), x.followUStore.end(), id)
+                    ,x.followUStore.end());
+        }
+
+
+    }
+
+    //Return number of ppl u follow
+    int numFollowing(){
+        return static_cast<int>(UfollowStore.size());
+    }
+
+    //Return number of ppl who follow u
+    int numFollowers(){
+        return static_cast<int>(followUStore.size());
+    }
+
+    //Return how much user we got
+    static int count() {
+        return counter;
+    }
+
+private:
+    int  id;
+    static int counter, idCounter;
+    //Vectors for saving ids
+    vector <int> UfollowStore, followUStore;
+
+};
+
+
+int Member::counter = 0;
+int Member::idCounter = 1;
