42e4625289c7fff7f1b933ddc78c92911737c8f0
Yarin Knafo <yarin665@gmail.com>
2018-04-26 14:16:49 +0300

    Update CircularInt.cpp

--- CircularInt.cpp
+++ CircularInt.cpp
@@ -1,7 +1,7 @@
 #include <string>
-#include <stdexcept>
+#include <sstream>
 #include <algorithm>
-#include <iostream>
+
 
 #include "CircularInt.hpp"
 
@@ -13,14 +13,50 @@ CircularInt::CircularInt(int newStart, int newEnd) {
     currentInt=newStart;
 }
 
+CircularInt::CircularInt(CircularInt &X) {
+    start=X.start;
+    end=X.end;
+    currentInt=X.start;
+}
+
 //Destructor
 CircularInt::~CircularInt() {
 
 }
 
-ostream& operator<<(ostream& output, const CircularInt& X) {
-    output << X.currentInt ;
-    return output;
+int rangeBetween(CircularInt &other, int temp){
+
+    if(temp>=other.start && temp<=other.end)
+        other.currentInt=temp;
+    else if(temp<other.start){
+        while(temp<other.start){
+            temp=temp+(other.end-other.start+1);
+        }
+        other.currentInt=temp;
+
+    }
+    else if(temp>other.end){
+
+        while(temp>other.end){
+            temp=temp-(other.end-other.start+1);
+        }
+
+        other.currentInt=temp;
+    }
+
+    return other.currentInt;
+
+}
+
+
+ostream& operator << (ostream& os, CircularInt const& ci){
+    os<<ci.currentInt;
+    return os;
+}
+istream& operator >> (istream& is,CircularInt& ci){
+    is>>ci.currentInt;
+    ci.currentInt=rangeBetween(ci,ci.currentInt);
+    return is;
 }
 
 bool CircularInt::operator >(const CircularInt &X) const {
@@ -41,21 +77,18 @@ bool CircularInt::operator <=(const CircularInt &X) const {
 }
 
 
-CircularInt& CircularInt::operator =( int x) {
-    this->currentInt=x%this->end;
-    return *this;
+
+
+CircularInt& operator+= ( CircularInt& x, int num){
+    CircularInt &temp (x);
+    temp.currentInt= rangeBetween(temp, temp.currentInt + num);
+    return temp;
 }
 
-CircularInt& CircularInt::operator +=( int x) {
-    this->currentInt=(this->currentInt+x)%this->end;
-    return *this;
-}
-
-CircularInt& CircularInt::operator -=( int x) {
-    this->currentInt=(this->currentInt-x)%this->end;
-    if(this->currentInt<0)
-        this->currentInt+=12;
-    return *this;
+CircularInt& operator-= ( CircularInt& x, int num){
+    CircularInt &temp (x);
+    temp.currentInt= rangeBetween(temp, temp.currentInt - num);
+    return temp;
 }
 
 CircularInt& CircularInt::operator *=( int x) {
@@ -63,20 +96,12 @@ CircularInt& CircularInt::operator *=( int x) {
     return *this;
 }
 
-CircularInt& CircularInt::operator /=( int x) {
-    if(this->currentInt%x==0){
-        this->currentInt=(this->currentInt/x)%this->end;
-        return *this;
-    }
-    else
-        throw;
-
+CircularInt& operator%= ( CircularInt& x, int num){
+    CircularInt &temp (x);
+    temp.currentInt= rangeBetween(temp, temp.currentInt % num);
+    return temp;
 }
 
-CircularInt& CircularInt::operator %=( int x) {
-    this->currentInt=(this->currentInt%x)%this->end;
-    return *this;
-}
 
 
 
@@ -93,12 +118,47 @@ CircularInt& CircularInt::operator++(int) {
     return temp;
 }
 
+//OK TILL HERE
 
 
-int CircularInt:: operator+( CircularInt& x) {
-    return (this->currentInt+x.currentInt)%this->end;
+CircularInt& operator+ (CircularInt& other, int num ){
+    CircularInt &other1 (other);
+    other1.currentInt =rangeBetween(other1,other1.currentInt+num);
+    return other1;
+
 }
 
+CircularInt& operator+ (int num, CircularInt& other ){
+    CircularInt &other1 (other);
+    other1.currentInt =rangeBetween(other1,other1.currentInt+num);
+    return other1;
+}
+
+CircularInt& operator+ (CircularInt& a,CircularInt& b ){
+    CircularInt &a1 (a);
+    a1.currentInt=rangeBetween(a1,a1.currentInt+b.currentInt);
+    return a1;
+}
+
+int CircularInt:: operator /( int x){
+    if(this->currentInt%x==0){
+        this->currentInt=(this->currentInt/x)%this->end;
+        return this->currentInt;
+    }
+    else{
+        stringstream ss;
+        ss << "There is no number x in {" << start << "," << end << "} such that x*" << x << "=" << currentInt;
+        string s = ss.str();
+        throw s;
+    }
+
+}
+
+CircularInt& operator-(int y, CircularInt&x) {
+    CircularInt &temp (x);
+    temp.currentInt= rangeBetween(temp, y - temp.currentInt);
+    return temp;
+}
 
 int CircularInt:: operator-() {
     int ans=this->currentInt*(-1);
@@ -108,24 +168,33 @@ int CircularInt:: operator-() {
     return ans%this->end;
 }
 
-
-int CircularInt:: operator /( int x){
-    if(this->currentInt%x==0){
-        this->currentInt=(this->currentInt/x)%this->end;
-        return this->currentInt;
-    }
-    else{
-        cout<<"There is no number x in {"<<this->start<<", "<<this->end<<
-                  "} such that x*"<<x<<"="<<this->currentInt;
-        //throw logic_error();
-    }
+CircularInt& operator - (CircularInt& other, int num ){
+    CircularInt &temp (other);
+    temp.currentInt= rangeBetween(temp, temp.currentInt - num);
+    return temp;
 
 }
-
-int operator-(int y, CircularInt&x) {
-    int ans=(y-x.currentInt)%x.end;
-    if(ans<=0)
-        ans+=x.end;
-    return ans;
+CircularInt& operator- ( CircularInt& a, CircularInt& b ){
+    CircularInt &a1 (a);
+    a1.currentInt= rangeBetween(a1, a1.currentInt + b.currentInt);
+    return a1;
 }
 
+CircularInt& operator* (  CircularInt& other, int num){
+    CircularInt &other1 (other);
+    int temp = other1.currentInt*num;
+    other1.currentInt = rangeBetween(other1,temp);
+    return other1;
+}
+
+CircularInt& operator* ( int num, CircularInt& other){
+    CircularInt &other1 (other);
+    other1.currentInt=rangeBetween(other1,other1.currentInt*num);
+    return other1;
+}
+
+CircularInt& operator* ( CircularInt& a,CircularInt& b ){
+    CircularInt &a1 (a);
+    a1.currentInt=rangeBetween(a1,a1.currentInt*b.currentInt);
+    return a1;
+}
